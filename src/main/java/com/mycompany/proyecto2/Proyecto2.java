89bc13be61f2c28117a10ab15f286d50d9ab6ca7
BeaCardozo <113352853+BeaCardozo@users.noreply.github.com>
2025-03-12 20:25:40 -0400

    proyecto creado

---
+++ src/main/java/com/mycompany/proyecto2/Proyecto2.java
@@ -0,0 +1,12 @@
+package com.mycompany.proyecto2;
+
+/**
+ *
+ * @author beacardozo
+ */
+public class Proyecto2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
